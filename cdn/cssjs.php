bd800516d748eeed5559b56882f843be89efe58e
Tank <62957183+tank-sys@users.noreply.github.com>
2022-02-14 14:28:37 +0700

    Add files via upload

---
+++ cdn/cssjs.php
@@ -0,0 +1,21 @@
+<?php
+$cssawal ='
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css">
+    <style>
+html, body {
+    height: 100%;
+}
+
+a:link {  text-decoration: none;} a:visited {  text-decoration: none; } a:hover {  text-decoration: underline; color: red;} a:active {  text-decoration: underline; }
+    </style>
+';
+
+$ahref = "a:link {text-decoration: none; color: #000000 !important;} a:visited {text-decoration: none; color: #000000 !important; } a:hover {text-decoration: underline; color: red;} a:active {text-decoration: none; color: #000000 !important;}";
+
+$jsawal ='
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/js/bootstrap.min.js"></script>
+';
